813bd5c6e0655e3495bdc7b48608a77c14421a49
Luna <luna@waya.one>
2025-01-18 18:37:59 +0100

    fix tailwind

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,5 +1,6 @@
 import {
     Links,
+    LinksFunction,
     Meta,
     Outlet,
     Scripts,
@@ -7,7 +8,21 @@ import {
 } from "react-router";
 
 import { Config } from "constants/config";
-import "./tailwind.css?url";
+import stylesheet from "./tailwind.css?url";
+
+export const links: LinksFunction = () => [
+    { rel: "stylesheet", href: stylesheet },
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossOrigin: "anonymous"
+    },
+    {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+    }
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
