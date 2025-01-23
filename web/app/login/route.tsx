dc0ebee63226ada13c206e288d0811a5d92dbead
Luna <luna@waya.one>
2025-01-23 20:17:03 +0100

    move auth layout to reusable component

--- web/app/login/route.tsx
+++ web/app/login/route.tsx
@@ -1,12 +1,13 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Turnstile } from '@marsidev/react-turnstile';
-import { Button } from "components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "components/ui/form";
-import { Input } from "components/ui/input";
-import { Tabs, TabsList, TabsTrigger } from "components/ui/tabs";
+import { Turnstile } from "@marsidev/react-turnstile";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { APIPostAuthLoginBodySchema, APIPostAuthLoginResponse, APIPostAuthRegisterBody, APIPostAuthRegisterBodySchema, APIPostAuthRegisterResponse } from "types/auth";
+import { Auth, AuthContent, AuthDescription, AuthTitle } from "~/components/auth";
+import { Button } from "~/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Tabs, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { Config } from "~/constants/config";
 import { request } from "~/lib/api";
 
@@ -21,7 +22,7 @@ const fields = [
     "password"
 ] as const;
 
-export default function Register() {
+export default function Login() {
     const [type, setType] = useState<Type>(Type.Login);
 
     const form = useForm<APIPostAuthRegisterBody>({
@@ -29,13 +30,7 @@ export default function Register() {
             type === Type.Create
                 ? APIPostAuthRegisterBodySchema
                 : APIPostAuthLoginBodySchema
-        ),
-        defaultValues: {
-            email: "",
-            username: "",
-            password: "",
-            captcha_key: ""
-        }
+        )
     });
 
     const email = form.watch("email");
@@ -68,78 +63,77 @@ export default function Register() {
     }
 
     return (
-        <div className="bg-[url('/background.avif')] bg-cover bg-no-repeat h-screen w-full flex justify-start p-24">
-            <div className="p-6 border bg-card/75 backdrop-blur border-muted rounded-lg h-fit min-w-[24rem]">
-                <h1 className="text-2xl font-medium ">👋 Welcome to {Config.platform_name}</h1>
-                <p className="text-sm text-muted-foreground">Tell us who you are to start chatting!</p>
-
-                <Tabs
-                    defaultValue={Type.Login}
-                    className="w-full mt-4 mb-2"
-                    onValueChange={(value) => setType(value as Type)}
-                >
-                    <TabsList className="grid w-full grid-cols-2">
-                        <TabsTrigger value={Type.Login}>Login</TabsTrigger>
-                        <TabsTrigger value={Type.Create}>Create</TabsTrigger>
-                    </TabsList>
-                </Tabs>
-
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(type === Type.Create ? register : login)}
-                        className="space-y-1"
+        <Auth>
+            <AuthTitle>👋 Welcome to {Config.platform_name}</AuthTitle>
+            <AuthDescription>Tell us who you are to start chatting!</AuthDescription>
+                <AuthContent>
+                    <Tabs
+                        defaultValue={Type.Login}
+                        className="w-full mt-5 mb-3"
+                        onValueChange={(value) => setType(value as Type)}
                     >
-                        {fields.map((id) =>
-                           (type === Type.Login && id === "username") ? null : (
-                            <FormField
-                                key={id}
-                                control={form.control}
-                                name={id}
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>
-                                            {id.replace(/^\w/, (char) => char.toUpperCase())}
-                                        </FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                type={id}
-                                                autoComplete={id === "password"
-                                                    ? (type === Type.Login ? "current-password" : "new-password")
-                                                    : id
-                                                }
-                                                autoFocus={id === "email"}
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                        ))}
-                        <div className="h-2" />
+                        <TabsList className="grid w-full grid-cols-2">
+                            <TabsTrigger value={Type.Login}>Login</TabsTrigger>
+                            <TabsTrigger value={Type.Create}>Create</TabsTrigger>
+                        </TabsList>
+                    </Tabs>
 
-                        {(canContinue || captchaKey) &&
-                            <Turnstile
-                            className="!mb-2"
-                                siteKey={Config.captcha_site_key}
-                                options={{
-                                    size: "flexible",
-                                    theme: "dark"
-                                }}
-                                onSuccess={(key) => form.setValue("captcha_key", key)}
-                            />
-                        }
-
-                        <Button
-                            type="submit"
-                            disabled={!canContinue || !captchaKey}
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(type === Type.Create ? register : login)}
+                            className="space-y-2"
                         >
-                            Submit
-                        </Button>
-                    </form>
-                </Form>
+                            {fields.map((id) =>
+                               (type === Type.Login && id === "username") ? null : (
+                                <FormField
+                                    key={id}
+                                    control={form.control}
+                                    name={id}
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>
+                                                {id.replace(/^\w/, (char) => char.toUpperCase())}
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    type={id}
+                                                    autoComplete={id === "password"
+                                                        ? (type === Type.Login ? "current-password" : "new-password")
+                                                        : id
+                                                    }
+                                                    autoFocus={id === "email"}
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            ))}
+                            <div className="h-2" />
 
-            </div>
-        </div>
+                            {(canContinue || captchaKey) &&
+                                <Turnstile
+                                    className="!mb-2"
+                                    siteKey={Config.captcha_site_key}
+                                    options={{
+                                        size: "flexible",
+                                        theme: "dark"
+                                    }}
+                                    onSuccess={(key) => form.setValue("captcha_key", key)}
+                                />
+                            }
+
+                            <Button
+                                className="w-full"
+                                type="submit"
+                                disabled={!canContinue || !captchaKey}
+                            >
+                                Submit
+                            </Button>
+                        </form>
+                    </Form>
+                </AuthContent>
+        </Auth>
     );
 }
---
+++ web/components/auth.tsx
@@ -0,0 +1,23 @@
+export function Auth({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="bg-[url('/background.avif')] bg-cover bg-no-repeat h-screen w-full flex items-center justify-start sm:p-32">
+            <div className="p-6 border bg-card/75 backdrop-blur border-muted rounded-lg h-full sm:h-fit w-full sm:w-fit min-w-[24rem]">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+//
+export function AuthTitle({ children }: { children: React.ReactNode }) {
+    return <h1 className="text-2xl font-medium ">{children}</h1>;
+}
+
+//
+export function AuthDescription({ children }: { children: React.ReactNode }) {
+    return <p className="text-sm text-muted-foreground">{children}</p>;
+}
+
+export function AuthContent({ children }: { children: React.ReactNode }) {
+    return <div className="mt-5">{children}</div>;
+}
