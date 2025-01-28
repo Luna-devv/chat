1efedcabf4d094f8cd55dedd01d79f4fc17ec2c1
Luna <luna@waya.one>
2025-01-28 17:49:38 +0100

    add redis pub(/sub) to api

---
+++ web/utils/emit-event.ts
@@ -0,0 +1,19 @@
+import Redis from "ioredis";
+
+import type { EventMap, GatewayMessage } from "~/types/gateway";
+
+const redis = new Redis();
+
+export function emitGatewayEvent<T extends keyof EventMap>(
+    channel: `user:${number}` | `server:${number}`,
+    event: T,
+    data: Parameters<EventMap[T]>[0]
+) {
+    void redis.publish(
+        channel,
+        JSON.stringify({
+            t: event,
+            d: data
+        } satisfies GatewayMessage)
+    );
+}
