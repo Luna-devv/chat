c4870e6afa936b627046d46877fd52d64d256b05
Luna <luni.siaa@gmail.com>
2025-02-01 16:09:26 +0100

    remove unused message submit form

--- web/app/(app)/rooms/[sid]/[rid]/route.tsx
+++ web/app/(app)/rooms/[sid]/[rid]/route.tsx
@@ -1,33 +1,10 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { useParams } from "react-router";
-
 import { MessageInput } from "~/components/rooms/message-input";
 import { MessageView } from "~/components/rooms/message-view";
-import { request } from "~/lib/api";
-import type { APIPostRoomMessagesBody, APIPostRoomMessagesResponse } from "~/types/messages";
-import { APIPostRoomMessagesBodySchema } from "~/types/messages";
 
 export default function Room() {
-    const params = useParams();
-
-    const form = useForm<APIPostRoomMessagesBody>({
-        resolver: zodResolver(APIPostRoomMessagesBodySchema)
-    });
-
-    async function handle(data: APIPostRoomMessagesBody) {
-        const res = await request<APIPostRoomMessagesResponse>("post", `/rooms/${params.rid}/messages`, data);
-
-        if ("message" in res) {
-            form.setError("content", { message: res.message });
-            return;
-        }
-    }
-
     return (
         <div className="flex flex-col h-screen w-full p-3">
             <MessageView />
-
             <MessageInput />
         </div>
     );
